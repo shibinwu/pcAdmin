785ebe5eb25b403a8ab569bdd25757c5692d95c2
wujianyin <13366941157@163.com>
2017-09-08 11:45:04 +0800

    aaa

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -7,9 +7,9 @@ return array(
     //mysql oracle mssql access db2 sqlite ...
     'DB_TYPE'               =>  'mysql',     // 数据库类型
     'DB_HOST'               =>  'localhost', // 服务器地址
-    'DB_NAME'               =>  'migan',          // 数据库名
+    'DB_NAME'               =>  'wnv',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  'root123',          // 密码
+    'DB_PWD'                =>  'root',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     // 'DB_PREFIX'             =>  'tp_',    // 数据库表前缀
 
