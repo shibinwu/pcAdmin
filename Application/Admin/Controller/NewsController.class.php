ebccabc9fa694997d9932e63e72487f4efe9de64
wujianyin <13366941157@163.com>
2017-09-14 19:46:53 +0800

    新闻的添加删除修改

--- Application/Admin/Controller/NewsController.class.php
+++ Application/Admin/Controller/NewsController.class.php
@@ -10,7 +10,7 @@ class NewsController extends CommonController{
 	#add方法，展示模版文件
 	public function add(){
 		#实例化模型
-		$model = M('Goods_attr');
+		$model = M('News_type');
 		#查询属性信息
 		$data = $model -> select();
 		#传递给模板
@@ -25,54 +25,57 @@ class NewsController extends CommonController{
 		$post = I('post.');
 //		dump($post);die;
 		#获取文件的数据
-		 $file = $_FILES['icon'];
-//		dump($_FILES);die;
-		#配置上传信息
-		 $cfg = array(
-		 		//保存根路径
-		 		'rootPath'      =>  WORKING_PATH . UPLOAD_ROOT_PATH,
-		 	);
-		#实例化上传类
-		 $uplaod = new \Think\Upload($cfg);
-		#上传操作
-		 if($file['size'] > 0){
-		 	$info = $uplaod -> uploadOne($file);//一维数组
-//			  dump($info);die;
-		 	#判断返回结果
-		 	if($info){
-		 		#hasfile字段
-		 		$post['hasfile'] = 1;
-		 		#filename字段
-		 		$post['filename'] = $info['savename'];
-		 		#filepath字段
-		 		$post['icon'] = UPLOAD_ROOT_PATH . $info['savepath'] . $info['savename'];
-		 	}
-		 }
+//		 $file = $_FILES['icon'];
+////		dump($_FILES);die;
+//		#配置上传信息
+//		 $cfg = array(
+//		 		//保存根路径
+//		 		'rootPath'      =>  WORKING_PATH . UPLOAD_ROOT_PATH,
+//		 	);
+//		#实例化上传类
+//		 $uplaod = new \Think\Upload($cfg);
+//		#上传操作
+//		 if($file['size'] > 0){
+//		 	$info = $uplaod -> uploadOne($file);//一维数组
+////			  dump($info);die;
+//		 	#判断返回结果
+//		 	if($info){
+//		 		#hasfile字段
+//		 		$post['hasfile'] = 1;
+//		 		#filename字段
+//		 		$post['filename'] = $info['savename'];
+//		 		#filepath字段
+//		 		$post['icon'] = UPLOAD_ROOT_PATH . $info['savepath'] . $info['savename'];
+//		 	}
+//		 }
 		// dump($post);die;
 		#添加addtime字段
-		 $post['ctime'] = time();
+		 $post['addtime'] = time();
 		#写入数据表
-		$model = M('Goods');
+		$model = M('News');
 		$rst = $model -> add($post);
 		#判断返回值
 		if($rst){
 			#成功
-			$this -> success('添加商品成功',U('showList'),3);
+			$this -> success('添加新闻成功',U('showList'),3);
 		}else{
 			#失败
-			$this -> error('添加商品失败',U('add'),3);
+			$this -> error('添加新闻失败',U('add'),3);
 		}
 	}
 
 	#showList方法，获取数据展示数据
 	public function showList(){
 		#获取数据
-		$model = M('Goods');
+		$model = M('News');
+		$models = M('News_type');
 		#查询
 		$data = $model -> where('statu = 1') -> select();
-//		dump($data);die;
+		$datas = $models -> select();
+//		dump($datas);die;
 		#传递变量给模版
 		$this -> assign('data',$data);
+		$this -> assign('datas',$datas);
 		#展示模版
 		$this -> display();
 	}
@@ -135,26 +138,17 @@ class NewsController extends CommonController{
 	 public function edit(){
 	 	#接收数据
 	 	$id = I('get.id');
-
 	 	#实例化模型
-	 	$model = M('Goods');
-//		 $data = $model -> field('t1.*,t2.ga_name as name') -> table('goods as t1,goods_attr as t2') -> where('t1.g_class = t2.ga_id') -> select();
-
-		 //todo 商品分类
-		 $gtmodel = M('goods_attr');
-		 $class = $gtmodel->where('ga_id','>','0')->where('ga_type = 2')->select();
-		 $grand = $gtmodel->where('ga_id','>','0')->where('ga_type = 3')->select();
-		 $position = $gtmodel->where('ga_id','>','0')->where('ga_type = 4')->select();
-
+	 	$model = M('News');
+		 $models = M (News_type);
 
 	 	#查询操作
 	 	$data = $model -> find($id);
+		 $datas = $models -> select();
 //		 dump($data);die;
 	 	#传递给模版
 	 	$this -> assign('data',$data);
-	 	$this -> assign('class',$class);
-	 	$this -> assign('grand',$grand);
-	 	$this -> assign('position',$position);
+	 	$this -> assign('datas',$datas);
 	 	#展示模版
 	 	$this -> display();
 	 }
@@ -193,8 +187,8 @@ class NewsController extends CommonController{
 		 $post['mtime'] = time();
 	 	#写入到数据表
 //		 dump($post);die;
-	 	$model = M('Goods');
-		 if($post['gid']){
+	 	$model = M('News');
+		 if($post['id']){
 			 $rst = $model -> save($post);
 		 }else{
 			 echo '123';
