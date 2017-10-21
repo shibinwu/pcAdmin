35d49fa4b8dce0d7b3736c583947bc428e3850ac
wujianyin <13366941157@163.com>
2017-10-21 14:45:59 +0800

    商品属性模块的bug修复

--- Application/Admin/Controller/GoodsattrController.class.php
+++ Application/Admin/Controller/GoodsattrController.class.php
@@ -5,8 +5,6 @@ namespace Admin\Controller;
 use Think\Controller;
 #声明类并且继承父类
 class GoodsattrController extends CommonController{
-
-
 	#add方法，展示模版文件
 	public function add(){
 		#实例化模型
@@ -23,43 +21,18 @@ class GoodsattrController extends CommonController{
 	public function addOk(){
 		#接收数据
 		$post = I('post.');
-//		dump($post);die;
-		#获取文件的数据
-		 $file = $_FILES['file'];
-		#配置上传信息
-		 $cfg = array(
-		 		//保存根路径
-		 		'rootPath'      =>  WORKING_PATH . UPLOAD_ROOT_PATH,
-		 	);
-		#实例化上传类
-		 $uplaod = new \Think\Upload($cfg);
-		#上传操作
-		 if($file['size'] > 0){
-		 	$info = $uplaod -> uploadOne($file);//一维数组
-//			 dump($info);die;
-		 	#判断返回结果
-		 	if($info){
-		 		#hasfile字段
-		 		$post['hasfile'] = 1;
-		 		#filename字段
-		 		$post['filename'] = $info['savename'];
-		 		#filepath字段
-		 		$post['filepath'] = UPLOAD_ROOT_PATH . $info['savepath'] . $info['savename'];
-		 	}
-		 }
-//		dump($post);die;
 		#添加addtime字段
-		 $post['ctime'] = time();
+	 	$post['ctime'] = time();
 		#写入数据表
 		$model = M('Goods_attr');
 		$rst = $model -> add($post);
 		#判断返回值
 		if($rst){
 			#成功
-			$this -> success('添加属性成功',U('showList'),3);
+			$this -> success('添加属性成功',U('showList'),1);
 		}else{
 			#失败
-			$this -> error('添加属性失败',U('add'),3);
+			$this -> error('添加属性失败',U('add'),1);
 		}
 	}
 
@@ -69,45 +42,15 @@ class GoodsattrController extends CommonController{
 		$model = M('Goods_attr');
 		#查询
 		$data = $model -> where('statu = 1') -> select();
-//		dump($data);die;
 		#传递变量给模版
 		$this -> assign('data',$data);
 		#展示模版
 		$this -> display();
 	}
-
-	#download方法，实现附件的下载
-	// public function download(){
-	// 	#接收id
-	// 	$id = I('get.id');
-	// 	#实例化
-	// 	$model = M('Doc');
-	// 	#查询
-	// 	$data = $model -> find($id);
-	// 	$file = WORKING_PATH . $data['filepath'];
-	// 	header("Content-type: application/octet-stream");
-	// 	header('Content-Disposition: attachment; filename="' . basename($file) . '"');
-	// 	header("Content-Length: ". filesize($file));
-	// 	readfile($file);
-	// }
-
-	// #getContent方法，输出指定公文的内容
-	// public function getContent(){
-	// 	#接收id
-	// 	$id = I('get.id');
-	// 	#实例化
-	// 	$model = M('Doc');
-	// 	#查询
-	// 	$data = $model -> find($id);
-	// 	#输出内容
-	// 	echo htmlspecialchars_decode($data['content']);
-	// }
-
     #del方法，实现删除
     public function del(){
         #接收参数
         $id = I('get.id');
-//        dump($id);die;
         #实例化模型
         $model = M('Goods_attr');
         #删除操作
@@ -118,28 +61,24 @@ class GoodsattrController extends CommonController{
             'statu'  => '0'
         );
         $rst =$model -> save($data);
-//        dump($rst);die;
         #判断返回值
         if($rst){
             #删除成功
-            $this -> success('删除成功',U('showList'),3);
+            $this -> success('删除成功',U('showList'),1);
         }else{
             #删除失败
-            $this -> error('删除失败',U('showList'),3);
+            $this -> error('删除失败',U('showList'),1);
         }
     }
-
 
     #edit方法
 	 public function edit(){
 	 	#接收数据
 	 	$id = I('get.id');
-
 	 	#实例化模型
 	 	$model = M('Goods_attr');
 	 	#查询操作
 	 	$data = $model -> find($id);
-//		 dump($data);
 	 	#传递给模版
 	 	$this -> assign('data',$data);
 	 	#展示模版
@@ -150,31 +89,7 @@ class GoodsattrController extends CommonController{
 	 public function editOk(){
 	 	#接收post数据
 	 	$post = I('post.');
-//	 	dump($post);die;
-	 	#判断是否有附件上传
-//	 	if($_FILES['file']['size'] > 0){
-//	 		#配置
-//	 		$cfg = array(
-//	 			'rootPath' => WORKING_PATH . UPLOAD_ROOT_PATH
-//	 			);
-//	 		#实例化
-//	 		$upload = new \Think\Upload($cfg);
-//	 		#上传操作
-//	 		$info = $upload -> uploadOne($_FILES['file']);
-//	 		#判断上传结果
-//	 		if($info){
-//	 			#上传成功
-//	 			#filepath字段
-//	 			$post['filepath'] = UPLOAD_ROOT_PATH . $info['savepath'] . $info['savename'];
-//	 			#filename字段
-//	 			$post['filename'] = $info['savename'];
-//	 			#hasfile字段
-//	 			$post['hasfile'] = 1;
-//	 			// $yuanshi = $model -> find($post['id']);
-//	 			// $path = WORKING_PATH . $yuanshi['filepath'];
-//	 			// unlink($path);
-//	 		}
-//	 	}
+	 	$psot['mtime'] = time();
 	 	#写入到数据表
 	 	$model = M('Goods_attr');
 		 if($post['ga_id']){
@@ -186,10 +101,10 @@ class GoodsattrController extends CommonController{
 	 	#判断返回结果
 	 	if($rst){
 	 		#成功
-	 		$this -> success('编辑成功',U('showList'),3);
+	 		$this -> success('编辑成功',U('showList'),1);
 	 	}else{
 	 		#失败
-	 		$this -> error('编辑失败',U('edit',array('id' => $post['id'])),3);
+	 		$this -> error('编辑失败',U('edit',array('id' => $post['id'])),1);
 	 	}
 	 }
 }
