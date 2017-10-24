a3443bd84808e71f41438f37f1e31c54b179d99d
wujianyin <13366941157@163.com>
2017-10-24 15:22:01 +0800

    添加商品

--- Application/Admin/Controller/GoodsController.class.php
+++ Application/Admin/Controller/GoodsController.class.php
@@ -9,33 +9,42 @@ class GoodsController extends CommonController{
 
 	#add方法，展示模版文件
 	public function add(){
-		//DOTA 一级属性
-		$data_one = M('Goods_attr')->where('pgaid=2')-> select();
-		$this -> assign('data_one',$data_one);
-		// var_dump($data_one);exit;
-		//CSGO 一级属性
-		$csgo_one = M('Goods_attr')->where('pgaid=1')-> select();
-		$this -> assign('csgo_one',$csgo_one);
-		
-		
-		#实例化模型
-		$model = M('Goods_attr');
-		#查询属性信息
-		$data = $model -> select();
-		#传递给模板
-		$this -> assign('data',$data);
+		//获取DOTA属性id和名称
+		$dota_data = M('Dota_attr')->select();
+		foreach($dota_data as $index=>$item){
+			$dota_attr[$item['id']] = $item;
+		}
+		//获取DOTA英雄id和名称
+		$hero_model = M('Dota_hero');
+		$hero_data = $hero_model -> select();
+		foreach($hero_data as $index=>$item){
+			$hero_attr[$item['id']] = $item;
+		}
+		$this -> assign('dota_attr',$dota_attr);
+		$this -> assign('hero_attr',$hero_attr);
+		//获取CSGO属性id和名称
+		$csgo_data = M('Csgo_attr')->select();
+		foreach($csgo_data as $index=>$item){
+			$csgo_attr[$item['id']] = $item;
+		}
+		//获取CSGO武器id和名称
+		$gun_model = M('Csgo_gun');
+		$gun_data = $gun_model -> select();
+		foreach($gun_data as $index=>$item){
+			$gun_attr[$item['id']] = $item;
+		}
+		$this -> assign('csgo_attr',$csgo_attr);
+		$this -> assign('gun_attr',$gun_attr);
 		#展示模版
 		$this -> display();
 	}
 
 	#addOk方法，接收数据保存数据
-	public function addOk(){
+	public function dotaAdd(){
 		#接收数据
 		$post = I('post.');
-//		dump($post);die;
 		#获取文件的数据
 		 $file = $_FILES['icon'];
-//		dump($file);die;
 		#配置上传信息
 		 $cfg = array(
 		 		//保存根路径
@@ -61,22 +70,67 @@ class GoodsController extends CommonController{
 		#添加addtime字段
 	 	$post['ctime'] = time();
 		#写入数据表
-		$model = M('Goods');
+		$model = M('Goods_new');
 		$rst = $model -> add($post);
 		#判断返回值
 		if($rst){
 			#成功
-			$this -> success('添加商品成功',U('showList'),3);
+			$this -> success('添加商品成功',U('showList'),1);
 		}else{
 			#失败
-			$this -> error('添加商品失败',U('add'),3);
+			$this -> error('添加商品失败',U('add'),1);
+		}
+	}
+
+
+	#addOk方法，接收数据保存数据
+	public function csgoAdd(){
+		#接收数据
+		$post = I('post.');
+		#获取文件的数据
+		$file = $_FILES['icon'];
+		#配置上传信息
+		$cfg = array(
+			//保存根路径
+				'rootPath'      =>  WORKING_PATH . UPLOAD_ROOT_PATH,
+		);
+		#实例化上传类
+		$uplaod = new \Think\Upload($cfg);
+		#上传操作
+		if($file['size'] > 0){
+			$info = $uplaod -> uploadOne($file);//一维数组
+//			  dump($info);die;
+			#判断返回结果
+			if($info){
+				#hasfile字段
+				$post['hasfile'] = 1;
+				#filename字段
+				$post['filename'] = $info['savename'];
+				#filepath字段
+				$post['icon'] = UPLOAD_ROOT_PATH . $info['savepath'] . $info['savename'];
+			}
+		}
+//		 dump($post);die;
+		#添加addtime字段
+		$post['ctime'] = time();
+		#写入数据表
+		$model = M('Goods_new');
+		$rst = $model -> add($post);
+		#判断返回值
+		if($rst){
+			#成功
+			$this -> success('添加商品成功',U('showList'),1);
+		}else{
+			#失败
+			$this -> error('添加商品失败',U('add'),1);
 		}
 	}
 
 	#showList方法，获取数据展示数据
 	public function showList(){
 		#获取数据
-		$model = M('Goods');
+		$model = M('Goods_new');
+
 		#查询
 		$data = $model -> where('statu = 1') -> select();
 //		dump($data);die;
